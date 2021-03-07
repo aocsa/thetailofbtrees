8cb744cfdc2fa1e18c9902db24867d6427cbdd8b
Alexander <aocsa.cs@gmail.com>
2021-03-07 18:28:59 -0500

    binary_tree

---
+++ golang/binary_tree.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"github.com/apache/arrow/go/arrow/memory"
+)
+import "github.com/gomem/gomem/pkg/dataframe"
+
+type Integer int
+type Float float64
+
+type QItem interface {
+	Less(QItem) bool
+}
+
+func (a Integer) Less(b QItem) bool {
+	if a < b.(Integer) {
+		return true
+	}
+	return false
+}
+
+func (a Float) Less(b QItem) bool {
+	if a < b.(Float) {
+		return true
+	}
+	return false
+}
+
+type Node struct {
+	value QItem
+	children []*Node
+}
+
+func MakeNode(val QItem) *Node {
+	return & Node{
+		value:val,
+		children:make([]*Node, 2),
+	}
+}
+
+type BinaryTree struct {
+	root *Node
+	height int
+}
+
+func MakeBinaryTree() *BinaryTree {
+	return &BinaryTree{
+		root: nil,
+		height: 0,
+	}
+}
+
+
+
+func recursiveInsert(parent *Node, value QItem) *Node {
+	if parent == nil {
+		return MakeNode(value)
+	} else {
+		whichIndex := 0
+		if value.Less(parent.value) {
+			whichIndex = 0
+		} else {
+			whichIndex = 1
+		}
+		child := recursiveInsert(parent.children[whichIndex], value)
+		if parent.children[whichIndex] == nil {
+			parent.children[whichIndex] = child
+		}
+		return parent
+	}
+}
+
+func (self *BinaryTree)Insert(value QItem) {
+	if self.root == nil {
+		self.root = MakeNode(value)
+	} else {
+		recursiveInsert(self.root, value)
+	}
+}
+
+func (self *BinaryTree) Print() {
+	if self.root != nil {
+		recursivePrint(self.root, 0)
+	}
+}
+
+func recursivePrint(parent *Node, level int) {
+	if parent != nil {
+		recursivePrint(parent.children[1], level + 1)
+		for i := 0; i < level; i++ {
+			fmt.Print("....")
+		}
+		fmt.Println(parent.value)
+		recursivePrint(parent.children[0], level + 1)
+	}
+}
+
+
+func main() {
+	bt := MakeBinaryTree()
+	bt.Insert(Float(5.0))
+	bt.Insert(Float(3))
+	bt.Insert(Float(7))
+	bt.Insert(Float(2))
+	bt.Insert(Float(4))
+	bt.Insert(Float(9))
+	bt.Insert(Float(6))
+	bt.Print()
+
+
+	pool := memory.NewGoAllocator()
+	df, _ := dataframe.NewDataFrameFromMem(pool, dataframe.Dict{
+		"col1": []int32{1, 2, 3, 4, 5},
+		"col2": []float64{1.1, 2.2, 3.3, 4.4, 5},
+		"col3": []string{"foo", "bar", "ping", "", "pong"},
+		"col4": []interface{}{2, 4, 6, nil, 8},
+	})
+	fmt.Println(df)
+	defer df.Release()
+}
